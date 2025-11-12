3f90e56d94166c14809ddbdbe92de9f96cbfe4ef
Leonardo Diaz <diazleonardo237@gmail.com>
2025-11-12 10:56:45 -0500

    modifique la variable

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ const app = express();
 dotenv.config();
 app.use(morgan('dev'));
 app.use(cors({
-  origin: process.env.CLIENT,
+  origin: process.env.CLIENT_URL,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true
 }));
