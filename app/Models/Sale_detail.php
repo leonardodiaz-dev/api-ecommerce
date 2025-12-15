122676e1a45e3760afe619365218ee71066799b6
Leonardo Diaz <diazleonardo237@gmail.com>
2025-12-14 21:59:37 -0500

    agrega la relacion con sale

--- app/Models/Sale_detail.php
+++ app/Models/Sale_detail.php
@@ -8,7 +8,15 @@ class Sale_detail extends Model
 {
     protected $fillable = ['sale_id','variant_id','cantidad','precio','descuento'];
 
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+    
     public function variant(){
         return $this->belongsTo(Variant::class);
+    }
+    public function sale(){
+        return $this->belongsTo(Sale::class);
     }
 }
