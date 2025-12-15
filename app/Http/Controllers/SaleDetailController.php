03c379b5b2a8f35d9e4f44d6d3c42185d048a647
Leonardo Diaz <diazleonardo237@gmail.com>
2025-12-14 21:58:39 -0500

    agrega listado de compras por usuario

--- app/Http/Controllers/SaleDetailController.php
+++ app/Http/Controllers/SaleDetailController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Sale;
 use App\Models\Sale_detail;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class SaleDetailController extends Controller
 {
@@ -13,6 +15,39 @@ class SaleDetailController extends Controller
     public function index()
     {
         //
+    }
+
+    public function getSalesWithDetailsByUser()
+    {
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+            $user_id = $user->id;
+            $sales = Sale_detail::whereHas('sale', function ($q) use ($user_id) {
+                $q->where('user_id', $user_id);
+            })
+                ->with(['variant.article'])
+                ->get()
+                ->map(function ($detail) {
+                    return [
+                        'id' => $detail->id,
+                        'cantidad' => $detail->cantidad,
+                        'precio'   => $detail->precio,
+                        'article'  => [
+                            'id'     => $detail->variant->article->id ?? null,
+                            'nombre' => $detail->variant->article->nombre ?? null,
+                            'imagen' => $detail->variant->article->imagen ?? null,
+                            'precio' => $detail->variant->article->precioVenta ?? null,
+                        ]
+                    ];
+                });
+
+            return response()->json($sales);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'message' => 'Error al procesar la solicitud para obtener las ventas',
+                'error' => $th->getMessage()
+            ], 500);
+        }
     }
 
     /**
