12e4e7fafced2828e670b37c2b68782e4dd144f7
Leonardo Diaz <diazleonardo237@gmail.com>
2025-12-14 22:02:54 -0500

    agrega nuevas rutas para cliente

--- routes/cliente.php
+++ routes/cliente.php
@@ -1,12 +1,14 @@
 <?php
 
 use App\Http\Controllers\AddressController;
+use App\Http\Controllers\SaleDetailController;
 use App\Http\Controllers\StripeController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\IsClienteAuth;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware([IsClienteAuth::class])->group(function () {
+  Route::get('sale-details', [SaleDetailController::class, 'getSalesWithDetailsByUser']);
   Route::post('stripe/create-session', [StripeController::class, 'createSession']);
   Route::patch('addresses/change/{id}', [AddressController::class, 'updateEstadoIsprincipal']);
   Route::resource('addresses', AddressController::class);
